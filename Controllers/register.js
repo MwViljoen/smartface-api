2c7b8c60063fab6dfb987dee5caddb77fd488566
Mw Viljoen <viljoenmw3@gmail.com>
2021-09-13 15:01:55 +0200

    debugging register error

--- Controllers/register.js
+++ Controllers/register.js
@@ -26,7 +26,8 @@ const handleRegister = (req, res, db, bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback)
     })
-        .catch((err) => res.status(400).json('Failed to register'));
+        .catch((err) => res.json(err));
+    /*res.status(400).json('Failed to register')*/
 }
 
 module.exports = {handleRegister}
