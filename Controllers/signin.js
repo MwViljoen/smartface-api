906989e1448dda0f41c1ff07e3828e42a663d973
Mw Viljoen <viljoenmw3@gmail.com>
2021-09-13 13:12:01 +0200

    updated for heroku deployment v1.0

--- Controllers/signin.js
+++ Controllers/signin.js
@@ -1,7 +1,7 @@
 const handleSignIn = (req, res, db, bcrypt) => {
     const {email, password} = req.body;
     if(!email || !password){
-        res.status(400).json('All fields required');
+        return res.status(400).json('All fields required');
     }
     db.select('email', 'hash').from('login')
         .where('email', '=', email)
